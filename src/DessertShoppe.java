5f9db892160b1739d0a0e33e4ba29acfa65a7581
RoseLu <qifei.lu1994@gmail.com>
2016-10-11 23:28:26 -0700

    Update DessertShoppe.java

--- src/DessertShoppe.java
+++ src/DessertShoppe.java
@@ -2,7 +2,7 @@
  * Created by qiqi on 2016/10/11.
  */
 public class DessertShoppe {
-    protected static final double TAX_RATE = 6.5;
+    public static final double TAX_RATE = 6.5;
     public static final String storeName = "M & M Dessert Shoppe";
     public int maximumSizeOfItemName = 50;
     public int widthOfCost = 8;
